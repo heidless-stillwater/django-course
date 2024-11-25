03b28363cbacc082ba7ca54c0ecc2d9b73a3afd0
heidless <lockhart.r@gmail.com>
2024-11-25 13:21:15 +0000

    lesson3 in process 01:33

--- lesson03/myproject/myproject/views.py
+++ lesson03/myproject/myproject/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
-def homepage(request):#
-  return HttpResponse("Hello world! I'm Home")
 
-def about(request):#
-  return HttpResponse("My About Page")
+def homepage(request):
+    # return HttpResponse("Hello World! I'm Home.")
+    return render(request, 'home.html')
 
+
+def about(request):
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')
